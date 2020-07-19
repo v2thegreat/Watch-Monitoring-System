e1a055c52d2ac37fc42e91a024ac1f5fc3d6531b
Ali Abbas <v2thegreat@gmail.com>
2020-07-19 13:00:39 -0400

    Added initial functionality to get data from server, bug fixes required

--- App/app/src/main/java/com/example/watchmonitoringapp/MainActivity.java
+++ App/app/src/main/java/com/example/watchmonitoringapp/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.watchmonitoringapp;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends WearableActivity {
 
     private TextView serverCount;
     private Button toastButton;
     private ProgressBar progressBar;
+    private RequestQueue requestQueue;
+
     private int i = 0;
 
     @Override
@@ -24,20 +40,43 @@ public class MainActivity extends WearableActivity {
         toastButton = findViewById(R.id.toastButton);
         progressBar = findViewById(R.id.progressBar);
 
+        requestQueue = Volley.newRequestQueue(this);
+
         toastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                test_updater();
+                jsonParse();
             }
         });
         // Enables Always-on
         setAmbientEnabled();
     }
 
-    private void test_updater(){
-        i += 10;
-        if (i >= 110)
-            i = 0;
-        progressBar.setProgress( i, true);
+    private void jsonParse() {
+        String url = "http://192.168.1.11:5555/minimal";
+//        String url = "http://www.google.com/";
+        Log.d("abcd", "Running3");
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, (JSONObject) null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("abcd", "Running2");
+                    JSONObject jsonObject = response.getJSONObject("abcd");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                5000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        requestQueue.add(request);
     }
 }
