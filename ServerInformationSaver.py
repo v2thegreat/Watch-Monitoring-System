2ca60ac5989bdbbfb95356b61f6feeefebc1269d
v2thegreat <v2thegreat@gmail.com>
2020-08-26 15:52:36 +0100

    Added more information and exception handling

--- ServerInformationSaver.py
+++ ServerInformationSaver.py
@@ -14,6 +14,10 @@ if __name__ == '__main__':
         current_datetime = datetime.now()
         sys_info = systemMonitoring.get_system_information()
         sys_info['datetime'] = current_datetime
-        db.insert_one(sys_info)
-        print(f'Saved data at: {current_datetime}')
+        try:
+            db.insert_one(sys_info)
+        except Exception as e: #TODO: Add more verbosity for exceptions
+            print(f"Exception: {e} was raised, skipping for this ping")
+        else:
+            print(f'Saved data at: {current_datetime}')
         sleep(10)
